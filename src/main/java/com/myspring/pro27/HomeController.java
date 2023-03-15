7e494d4dd2817e22fcb4c2e50e849ed7eef73b1c
hayaboy <hayaboy@korea.ac.kr>
2023-03-15 11:14:18 +0900

    3/15 멤버 추가

--- src/main/java/com/myspring/pro27/HomeController.java
+++ src/main/java/com/myspring/pro27/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+/* @Controller */
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
